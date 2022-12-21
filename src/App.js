8a245d605fdeb972f144084f0f50441570f917f5
unknown <sanjaymaharjan683@gmail.com>
2022-12-21 15:01:31 +0545

    latest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+const books = [
+  {id:1,
+    name:"Nepal",
+    author:"hari",
+},
+  {
+    id:2,
+    name:"Nepals",
+    author:"haris",
+},
+  ];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<section>
+    {/* {books.map((book) => {
+      return(<Test title={book.name} author={book.author} />);
+    })} */}
+    <Test key={books.id} datas={books}  />
+    </section>
+  );
+}
+const Test =(datas)=>{
+  const Example =(author)=>{
+    console.log(author)
+  }
+  console.log("data",datas.datas)
+  return(
+    <div>
+    {datas.datas.map((book) => {
+      return(
+    <div key={book.id}><h1
+      style={{ color: "red", fontSize: "4rem" }}
+    >{book.name}</h1><p>
+      {book.author}<br /> 
+      </p>
+      <button type="button" onClick={()=>Example(book.author)}>Example</button>
+    </div>);
+    })}
     </div>
   );
 }
